042b9f7fbb3e20fb7894d019bf64359d60a34761
thinca <thinca@gmail.com>
2022-07-10 12:21:42 +0900

    Use `cp.execFile()` instead of `cp.spawn()`

--- src/exec_vim.ts
+++ src/exec_vim.ts
@@ -1,32 +1,21 @@
-import {spawn} from "child_process";
+import {execFile} from "child_process";
 
 export function execVim(vimBin: string, commands: string[]): Promise<string> {
   return new Promise((resolve, reject) => {
-    const vim = spawn(vimBin, [
+    const vim = execFile(vimBin, [
       "-u", "NONE", "-i", "NONE", "-N",
       "-Z", "-X", "-R", "-e", "-s"
-    ]);
-    const resultBuffers: Buffer[] = [];
-    const errorBuffers: Buffer[] = [];
-    vim.stdout.on("data", (data) => {
-      resultBuffers.push(data);
-    });
-    vim.stderr.on("data", (data) => {
-      errorBuffers.push(data);
-    });
-    vim.on("exit", (exitCode) => {
-      const resultText = Buffer.concat(resultBuffers).toString();
-      const errorText = Buffer.concat(errorBuffers).toString();
-      if (exitCode === 0 && errorText === "") {
-        resolve(resultText.trimEnd());
+    ], (error, resultText, errorText) => {
+      if (error) {
+        reject({exitCode: error.code, resultText, errorText});
       } else {
-        reject({exitCode, resultText, errorText});
+        resolve(resultText.trimEnd());
       }
     });
     const script = commands
       .map((c) => c.replace(/\r?\n[^]*/, ""))
       .filter((c) => /\S/.test(c))
       .map((c) => `${c}\n`).join("");
-    vim.stdin.write(script);
+    vim.stdin!.write(script);
   });
 }
