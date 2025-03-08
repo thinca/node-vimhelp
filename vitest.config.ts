eb1fc78cdf8c93c4221a9e696f60ba855fe00bb4
Omochice <mochice.mls.ntl@gmail.com>
2025-03-09 01:06:14 +0900

    refactor: move option into a file

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    coverage: {
+      enabled: true,
+      include: ["src"],
+      clean: true,
+      provider: "istanbul",
+      reporter: ["lcovonly", "text"],
+      reportOnFailure: true,
+    },
+  },
+});
